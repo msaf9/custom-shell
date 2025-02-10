b5e905a74ec5b6a7f683e525cc88ab6bd17e73ce
sahil afrid farookhi <msafarookhi@gmail.com>
2025-02-09 22:18:20 -0600

    add code comments to Constants.c and Message.c

--- src/Constants.c
+++ src/Constants.c
@@ -1,3 +1,6 @@
+// This file defines constant error messages used in the custom shell program.
+// These messages are displayed to the user when specific syntax errors or issues occur.
+
 const char *SYNTAX_ERROR_LESS_THAN_MESSAGE = "Syntax error: expected input file after '<'\n";
 const char *SYNTAX_ERROR_GREATER_THAN_MESSAGE = "Syntax error: expected output file after '>'\n";
 const char *MISSING_ARGUMENT_MESSAGE = "cd: missing argument\n";
--- src/Message.c
+++ src/Message.c
@@ -1,3 +1,18 @@
+/**
+ * @file Message.c
+ * @brief This file contains the implementation of a simple message printing function.
+ */
+
+/**
+ * @brief Prints a message to the specified output stream.
+ *
+ * This function takes a file pointer and a prompt string as arguments,
+ * and prints the prompt string to the specified output stream.
+ *
+ * @param output A pointer to a FILE object that identifies the output stream.
+ * @param prompt A constant character pointer to the message string to be printed.
+ */
+
 #include <stdio.h>
 
 void message(FILE *output, const char *prompt)
